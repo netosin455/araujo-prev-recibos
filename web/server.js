c40813c8319ba9e11acff21daa05bc36f6b79ceb
Carlo Pegoraro <carlospegoraro926@gmail.com>
2026-06-12 14:32:57 -0300

    fix: CSP bloqueava CSS inline - adiciona 'unsafe-inline' em style-src

--- web/server.js
+++ web/server.js
@@ -617,7 +617,7 @@ app.use((req, res, next) => {
   res.setHeader("Content-Security-Policy",
     "default-src 'self'; " +
     "script-src 'self'; " +
-    "style-src 'self' https://fonts.googleapis.com https://cdn.jsdelivr.net; " +
+    "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.jsdelivr.net; " +
     "font-src 'self' https://fonts.gstatic.com https://cdn.jsdelivr.net; " +
     "img-src 'self' data: blob: https:; " +
     "connect-src 'self'; " +
